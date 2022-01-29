a095a886984488eaea1f64ea3cc6b3f11a669ba8
KwameBuadu <98181563+KwameBuadu@users.noreply.github.com>
2022-01-29 16:16:51 -0700

    new file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,31 @@
+//Kwame Buadu 
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let wakeup = "I woke up from a deep sleep";
+let charactername = "Unknown";
+let story = "Unknown Destination";
+let origin = "Capsule in the middle of the ocean on Earth";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+
+let age = 1000;
+let year = 2935;
+let century = 30;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+let items = ["broken time-travel device", "wrench"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+let isAlive = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("Hello. My name is " + charactername + " " + wakeup + " " + ".Right now, I am stuck in a " + origin);
+console.log(" Right now, I can't remember my age, but I am about " + age + " Years old, give or take. " + " It is " + year + " " + " in the " + century + "th" + " century " + " and, all of humanity, except me, has been wiped off the face of the earth ");
+console.log("Since I've been in this capsule, I've had a " + items[0] + " and " + items[1]);
+console.log("It is " + isAlive + " that I am still alive. Maybe if I can somehow fix my " + items[0] + " maybe I can veer away from this " + story + " im headed towards, travel back in time, and save humanity");
